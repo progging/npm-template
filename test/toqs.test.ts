875568a0ec8cd3e489588af97757caddae2e0746
Robin <rob@tenk.dev>
2020-08-18 21:48:21 +0200

    Template up!

---
+++ test/toqs.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import main from '../src/main'
+
+describe('npm-template', () => {
+  it('should test something', function () {
+    expect(main()).to.equal('TODO')
+  })
+})
